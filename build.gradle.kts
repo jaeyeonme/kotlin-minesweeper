1a7b1986c02c3685239c7d7b23528b057d0a1958
Jaeyeon Cho <cjyeon1022@gmail.com>
2026-09-03 10:55:01 +0900

    build: upgrade to JDK 25 and Kotlin 2.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    kotlin("jvm") version "2.4.10"
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
 }
 
 group = "camp.nextstep.edu"
@@ -11,22 +11,19 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
-    testImplementation("org.assertj", "assertj-core", "3.22.0")
-    testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("io.kotest:kotest-runner-junit5:5.2.3")
 }
 
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
-    test {
-        useJUnitPlatform()
-    }
-    ktlint {
-        verbose.set(true)
-    }
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+ktlint {
+    verbose.set(true)
 }
