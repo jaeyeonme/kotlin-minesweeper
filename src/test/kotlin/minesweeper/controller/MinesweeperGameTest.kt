681f81774ebde2fdd61d4f18a8cb90a8efa45703
Jaeyeon Cho <cjyeon1022@gmail.com>
2026-09-03 13:18:03 +0900

    test: cover continued gameplay turns

--- src/test/kotlin/minesweeper/controller/MinesweeperGameTest.kt
+++ src/test/kotlin/minesweeper/controller/MinesweeperGameTest.kt
@@ -11,6 +11,27 @@ import java.util.ArrayDeque
 
 class MinesweeperGameTest {
     @Test
+    fun `안전 칸을 연 뒤 게임이 진행 중이면 변경된 보드를 출력하고 다음 좌표를 입력받는다`() {
+        val inputs = ArrayDeque(listOf("2", "2", "1", "1, 1", "2, 2"))
+        val outputs = mutableListOf<String>()
+        val game = createGame(inputs, outputs)
+
+        game.start(fixedPlacement(Position(1, 1)))
+
+        assertThat(outputs)
+            .containsExactly(
+                "높이를 입력하세요.",
+                "너비를 입력하세요.",
+                "지뢰는 몇 개인가요?",
+                "지뢰찾기 게임 시작",
+                "open:",
+                "1 C\nC C",
+                "open:",
+                "Lose Game.",
+            )
+    }
+
+    @Test
     fun `마지막 안전 칸을 열면 변경된 보드를 출력하고 좌표 입력을 종료한다`() {
         val inputs = ArrayDeque(listOf("1", "2", "1", "1, 1"))
         val outputs = mutableListOf<String>()
